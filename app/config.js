d523a360738f4d888f4e4cfe93e28059fd2496c1
Qawelesizwe <qawemlilo@gmail.com>
2012-12-04 10:24:09 +0200

    writing better documantation

--- app/config.js
+++ app/config.js
@@ -4,6 +4,10 @@ var config = {
         host: 'localhost'
     },
     
+    email: 'qawemlilo@gmail.com',
+    
+    password: 'qerlkxveorabgzrg',
+    
     secret: '076ee61d63aa10a125ea872411e433b9'
 };
 
